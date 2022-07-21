6d965c273ce1e95dc14c08028db8d5814e7f926c
pentiminax <tanguy.lemarie.youtube@outlook.fr>
2022-07-21 16:46:50 +0200

    Created empty project

---
+++ FoodSearchTutorial/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FoodSearchTutorial
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FoodSearchTutorial/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FoodSearchTutorial
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
