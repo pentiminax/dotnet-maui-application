a50100f4efd055a6c3388a316a0078480328da89
pentiminax <tanguy.lemarie.youtube@outlook.fr>
2022-07-21 14:07:51 +0200

    Update ProductService and csproj file

--- FoodSearch/Services/ProductService.cs
+++ FoodSearch/Services/ProductService.cs
@@ -5,8 +5,7 @@ namespace FoodSearch.Services;
 
 public class ProductService
 {
-    const string BASE_URL = "https://world.openfoodfacts.org";
-    const string BASE_SEARCH_URL = "https://fr.openfoodfacts.org/cgi/search.pl";
+    const string BASE_SEARCH_URL = "https://fr.openfoodfacts.org/cgi/search.pl?action=process&json=true";
 
     HttpClient httpClient;
 
@@ -19,7 +18,7 @@ public class ProductService
     {
         var page = new Random().Next(1, 1000);
 
-        var url = $"{BASE_URL}?json=true&page={page}&page_size=10&{ProductService.GetNutriScoreFilter(0)}";
+        var url = $"{BASE_SEARCH_URL}&page={page}&page_size=10&{ProductService.GetNutriScoreFilter(0)}";
 
         var response = await httpClient.GetAsync(url);
 
@@ -31,8 +30,7 @@ public class ProductService
     public async Task<List<Product>> SearchProductsAsync(string searchTerm)
     {
 
-        var url = $"{BASE_SEARCH_URL}?action=process&json=true" +
-            $"&tag_contains_0=contains&tagtype_0=categories" +
+        var url = $"{BASE_SEARCH_URL}&tag_contains_0=contains&tagtype_0=categories" +
             $"&tagtype_1=label&tag_0={searchTerm}&{ProductService.GetNutriScoreFilter(2)}&page_size=10";
 
         var response = await httpClient.GetAsync(url);
